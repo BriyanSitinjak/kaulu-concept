07f025294a9df943f6ec48002c5ea56b8da12ad0
BriyanSitinjak <Imanbriyan@gmail.com>
2025-11-16 23:56:18 +0700

    export project

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // recommended for static hosting
 };
 
 export default nextConfig;
